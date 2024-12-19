c53171d85e316daa604fed80c4aadd919eb41941
vinith555 <159615639+vinith555@users.noreply.github.com>
2024-12-19 22:38:01 +0530

    Adding size, quatity and image navigation section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,16 +11,16 @@ import { ActivatedRoute, Router,RouterLink } from '@angular/router';
 export class HomeComponent {
   constructor(private route:Router,private activeRoute:ActivatedRoute){}
 productList:{img:string,name:string,price:number}[] = [
-  {img:'img1.avif',name:'Organized Clothes Rack',price:1200},
-  {img:'img2.avif',name:'Casual Outfit Flatlay',price:2499},
-  {img:'img3.avif',name:'Hanging Clothes Display',price:1800},
-  {img:'img4.avif',name:'Cozy Neutral Wardrobe',price:1999},
-  {img:'img5.avif',name:'Minimalist Chair Setup',price:1299},
-  {img:'img6.avif',name:'Stylish Accessories Flatlay',price:1499},
-  {img:'img7.avif',name:'Bold Red Outfit',price:2999},
-  {img:'img8.avif',name:'Stacked Denim Jeans',price:1699},
-  {img:'img9.avif',name:'Modern Outfit Essentials',price:2799},
-  {img:'img10.avif',name:'Trendy Sneakers Display',price:2499}
+  {img:'img1.jpg',name:'Classic maroon t-shirt',price:1200},
+  {img:'img2.jpg',name:'Trendy gray hoodie',price:2499},
+  {img:'img3.jpg',name:'Modern stylish sneakers',price:1800},
+  {img:'img4.jpg',name:'Checkered green shirt',price:1999},
+  {img:'img5.jpg',name:'Classic gray jeans',price:1299},
+  {img:'img6.jpg',name:'Maroon casual shirt',price:1499},
+  {img:'img7.jpg',name:'Distressed blue jeans',price:2999},
+  // {img:'img8.avif',name:'Stacked Denim Jeans',price:1699},
+  // {img:'img9.avif',name:'Modern Outfit Essentials',price:2799},
+  // {img:'img10.avif',name:'Trendy Sneakers Display',price:2499}
 ]
 navigateProduct(name:string,value:number){
   this.route.navigate(['product',name,value]);
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,21 +10,32 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class ProductComponent implements OnInit{
   productList:{img:string,name:string,price:string,images:string[]}[] = [
-    {img:'img1.avif',name:'Organized Clothes Rack',price:'₹1,200',images:['img1.avif','img1.1.jpeg','img1.2.jpeg','img1.3.jpeg','img1.4.jpeg']},
-    // {img:'img2.avif',name:'Casual Outfit Flatlay',price:'₹2,499'},
-    // {img:'img3.avif',name:'Hanging Clothes Display',price:'₹1,800'},
-    // {img:'img4.avif',name:'Cozy Neutral Wardrobe',price:'₹1,999'},
-    // {img:'img5.avif',name:'Minimalist Chair Setup',price:'₹1,299'},
-    // {img:'img6.avif',name:'Stylish Accessories Flatlay',price:'₹1,499'},
-    // {img:'img7.avif',name:'Bold Red Outfit',price:'₹2,999'},
-    // {img:'img8.avif',name:'Stacked Denim Jeans',price:'₹1,699'},
+    {img:'img1.jpg',name:'Classic maroon t-shirt',price:'₹1,200',images:['img1.jpg','img1.1.jpg','img1.2.jpg','img1.3.jpg']},
+    {img:'img2.jpg',name:'Trendy gray hoodie',price:'₹1,800',images:['img2.jpg','img2.1.jpg','img2.2.jpg']},
+    {img:'img3.jpg',name:'Modern stylish sneakers',price:'₹1,999',images:['img3.jpg','img3.1.jpg','img3.4.jpg','img3.3.jpg']},
+    {img:'img4.jpg',name:'Checkered green shirt',price:'₹1,299',images:['img4.jpg','img4.1.jpg','img4.2.jpg','img4.3.jpg']},
+    {img:'img5.jpg',name:'Classic gray jeans',price:'₹1,499',images:['img5.jpg','img5.1.jpg','img5.2.jpg','img5.3.jpg','img5.4.jpg']},
+    {img:'img6.jpg',name:'Maroon casual shirt',price:'₹2,999',images:['img6.jpg','img6.1.jpg','img6.2.jpg','img6.3.jpg','img6.4.jpg']},
+    {img:'img7.jpg',name:'Distressed blue jeans',price:'₹1,699',images:['img7.jpg','img7.1.jpg','img7.2.jpg','img7.3.jpg','img7.4.jpg']},
     // {img:'img9.avif',name:'Modern Outfit Essentials',price:'₹2,799'},
     // {img:'img10.avif',name:'Trendy Sneakers Display',price:'₹2,499'}
   ]
+  
+  sizeList:string[]=['S','M','L','XL','2XL'];
+  sizeValue:string='';
+
+  count:number = 0;
+  increment(){this.count++;}
+  decrement(){
+    if(this.count>0){
+      this.count--
+    }
+  }
   constructor(private route:ActivatedRoute,private renderer: Renderer2){}
   index!:number;
   productImg:string = '';
   currentElement!: HTMLElement;
+  currentElement1!:HTMLElement;
   loadImage(value:string,reference:HTMLImageElement){
     this.productImg = value;
     if (this.currentElement) {
@@ -42,6 +53,24 @@ export class ProductComponent implements OnInit{
           console.error('Invalid index');
         }}
       );
+  }
+  
+  setSize(value:string,reference:HTMLButtonElement){
+    this.sizeValue = value;
+    if(this.currentElement1){
+      this.renderer.setStyle(this.currentElement1,'background','white');
+      this.currentElement1.style.color = 'black';
+    }
+    this.renderer.setStyle(reference,'background','black');
+    reference.style.color = 'white';
+
+    this.currentElement1 = reference;
+  }
+
+  addingToCart(index:number){
+    console.log(this.count);
+    console.log(this.sizeValue);
+    console.log(index);
   }
 
 }
